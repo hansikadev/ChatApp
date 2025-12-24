ceb2d05d0dca30c3b8e1176d01a02060ea09941b
hansikadev <tanwarhansika123@gmail.com>
2025-12-24 19:25:46 +0530

    completed the component chatcontainer

--- client/src/components/Chatcontainer.jsx
+++ client/src/components/Chatcontainer.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect } from "react";
 import assets,{messagesDummyData} from "../assets/assets.js";
+import { formatMessageTime } from "../lib/util.js";
 
 const Chatcontainer = ({ selectedUser, setSelectedUser }) => {
   const scrollEnd = useRef();
@@ -8,7 +9,7 @@ const Chatcontainer = ({ selectedUser, setSelectedUser }) => {
   useEffect(() => {
     scrollEnd.current?.scrollIntoView({ behavior: "smooth" });
   }, [selectedUser]);
-  
+
   return selectedUser ? (
     <div className="h-full overflow-scroll relative backdrop-blur-lg">
       {/* Header */}
@@ -64,12 +65,39 @@ const Chatcontainer = ({ selectedUser, setSelectedUser }) => {
                 alt=""
                 className="w-7 rounded-full"
               />
-              <p className="text-gray-500">{msg.createdAt}</p>
+              <p className="text-gray-500">
+                {formatMessageTime(msg.createdAt)}
+              </p>
             </div>
           </div>
         ))}
         {/* Scroll Anchor */}
         <div ref={scrollEnd}></div>
+      </div>
+      {/* ------ bottom area ------ */}
+      <div className="absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3">
+        <div className="flex-1 flex items-center bg-gray-100/12 px-3 rounded-full">
+          <input
+            type="text"
+            placeholder="Send a message"
+            className="flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400"
+          />
+
+          {/* Hidden file input for images */}
+          <input type="file" id="image" accept="image/png, image/jpeg" hidden />
+
+          {/* Label linked to the hidden file input */}
+          <label htmlFor="image">
+            <img
+              src={assets.gallery_icon}
+              alt=""
+              className="w-5 mr-2 cursor-pointer"
+            />
+          </label>
+        </div>
+
+        {/* Send Button */}
+        <img src={assets.send_button} alt="" className="w-7 cursor-pointer" />
       </div>
     </div>
   ) : (
