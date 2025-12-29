f6ddb65028857f5b2611b8dc5d2d30b553355af0
hansikadev <tanwarhansika123@gmail.com>
2025-12-29 16:06:54 +0530

    setting up the connection between frontend and backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Homepage from './pages/homepage'
-import Loginpage from './pages/Loginpage'
-import Profilepage from './pages/Profilepage'
+import React, { useContext } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Homepage from "./pages/homepage";
+import Loginpage from "./pages/Loginpage";
+import Profilepage from "./pages/Profilepage";
+import { Toaster } from "react-hot-toast";
+import { AuthContext } from "../context/AuthContext";
+
 
 const App = () => {
+  const authContext = useContext(AuthContext);
+  const authUser = authContext?.authUser;
+  
   return (
     <div className="bg-[url('./src/assets/bgimage.jpg')] bg-contain ">
+      <Toaster />
       <Routes>
-        <Route path='/' element={<Homepage />} />
-        <Route path='/login' element={<Loginpage/>} />
-        <Route path='/profile' element={<Profilepage/>} />
-
+        <Route
+          path="/"
+          element={authUser ? <Homepage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={authUser ? <Navigate to="/" /> : <Loginpage />}
+        />
+        <Route
+          path="/profile"
+          element={authUser ? <Profilepage /> : <Navigate to="/login" />}
+        />
       </Routes>
-      
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
