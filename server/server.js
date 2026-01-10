6b03191e0803dc73e51280850b8d78c52ff742c9
hansikadev <tanwarhansika123@gmail.com>
2026-01-10 22:36:24 +0530

    build fix

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const server = http.createServer(app);
 // CORS CONFIGURATION
 // ======================
 const corsOptions = {
-  origin: "http://localhost:5173", // frontend (Vite)
+  origin: ["http://localhost:5173", "https://chat-app-in3a.vercel.app/login"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
@@ -35,7 +35,7 @@ app.use(express.json({ limit: "4mb" }));
 // ======================
 export const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://chat-app-in3a.vercel.app/login"],
     methods: ["GET", "POST"],
     credentials: true,
   },
