20d1d344f42f7424dbbc693d643f71def9d8ef74
hansikadev <tanwarhansika123@gmail.com>
2026-01-17 10:46:52 +0530

    route change

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ app.use(express.json({ limit: "4mb" }));
 // ======================
 export const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173","https://chat-app-in3a.vercel.app/login"],
+    origin: ["http://localhost:5173","https://chat-app-in3a.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true,
   },
